04a600d847115342b7718231405e887c72f96f34
Oyat0824 <langue0824@gmail.com>
2022-10-05 19:42:06 +0900

    기본적인 게시물 작성 및 리스트 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,10 +1,14 @@
 package com.KoreaIT.example.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		List<Article> articles = new ArrayList<Article>();
+		int lastArticleId = 0;
 		
 		System.out.println("JAM 시스템에 오신것을 환영합니다, 명령어를 입력해주세요.\n");
 		
@@ -17,7 +21,34 @@ public class Main {
 				break;
 			}
 			
-			
+			if(cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==\n");
+				int id = lastArticleId + 1;
+				
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				
+				lastArticleId++;
+
+				System.out.printf("[✔] %d번 글이 생성 되었습니다.\n", id);
+			}
+			else if(cmd.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==\n");
+				if(articles.isEmpty()) {
+					System.out.println("[✖] 게시글이 존재하지 않습니다.");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목	");
+				for(Article article : articles) {
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+			}
 		}
 		
 		sc.close();
